7d5fba7f5c780d1d27b327a56a3037e93e779694
Romain Vimont <rom@rom1v.com>
2023-10-24 12:30:49 +0200

    Use kynet

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,7 +1,6 @@
 use std::error::Error;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::time::Instant;
-use quinn::{ClientConfig, Endpoint};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -9,23 +8,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let cert_der = tokio::fs::read("tmp.cert").await?;
 
-    let certificate = rustls::Certificate(cert_der);
-    let mut certs = rustls::RootCertStore::empty();
-    certs.add(&certificate)?;
-
-    let client_config = ClientConfig::with_root_certificates(certs);
-
-    let endpoint = {
-        let bind_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0);
-        let mut endpoint = Endpoint::client(bind_addr)?;
-        endpoint.set_default_client_config(client_config);
-        endpoint
-    };
-
+    let certificate = kynet::quinn::Certificate::new(cert_der);
     let server_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 1234);
+    let conn = kynet::Connection::quinn_connect(
+        server_addr,
+        "localhost",
+        certificate,
+        &Default::default(),
+    ).await?;
 
-    let conn = endpoint.connect(server_addr, "localhost")?.await?;
-    println!("[client] [{}ms] connected: addr={}", start.elapsed().as_millis(), conn.remote_address());
+    //println!("[client] [{}ms] connected: addr={}", start.elapsed().as_millis(), conn.remote_address());
+    println!("[client] [{}ms] connected", start.elapsed().as_millis());
 
     let mut send = conn.open_uni().await?;
     println!("[client] [{}ms] stream opened", start.elapsed().as_millis());
@@ -35,11 +28,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     send.write_all("Hello, world!".as_bytes()).await?;
     println!("[client] [{}ms] data sent", start.elapsed().as_millis());
 
-    send.finish().await?;
+    send.close().await?;
 
-    conn.close(0u32.into(), b"done");
-
-    endpoint.wait_idle().await;
+    conn.close(0u32, "done");
 
     Ok(())
 }
